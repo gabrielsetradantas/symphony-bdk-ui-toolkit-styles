5d15bffde84edf40fd81b0cc2b9b9f46984802a9
gabriel.dantas <gabriel.dantas@symphony.com>
2021-07-08 12:11:20 +0200

    test

---
+++ creevey.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    // Pixelmatch options
+    diffOptions: { threshold: 1.0 },
+    // How many times test should be retried before to consider it as failed
+    maxRetries: 10,
+
+};
